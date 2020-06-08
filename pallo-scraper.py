9d9856bb89e42471804987cde2c8af42b5689ee7
Antti Oksman <antti.oksman101@gmail.com>
2020-06-09 01:02:46 +0300

    New scraper using the API from palloliitto.fi

--- pallo-scraper.py
+++ pallo-scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as BS
 base_url = "https://www.palloliitto.fi"
 scrape_url = "https://www.palloliitto.fi/seura/3436"
 crest = "https://ucdn.torneopal.fi/logo/palloliitto/3436x.png"
+match_data_url = "https://www.palloliitto.fi/get-match-data"
 all_ottelut = {}
 
 def scrape_ottelut(url):
@@ -44,6 +45,36 @@ def scrape_venue_details(url):
     #   "credentials": "include"
     # });
 
+    # curl
+    # 'https://www.palloliitto.fi/get-match-data' \
+    # - H
+    # 'authority: www.palloliitto.fi' \
+    # - H
+    # 'accept: application/json, text/javascript, */*; q=0.01' \
+    # - H
+    # 'x-requested-with: XMLHttpRequest' \
+    # - H
+    # 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36' \
+    # - H
+    # 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
+    # - H
+    # 'origin: https://www.palloliitto.fi' \
+    # - H
+    # 'sec-fetch-site: same-origin' \
+    # - H
+    # 'sec-fetch-mode: cors' \
+    # - H
+    # 'sec-fetch-dest: empty' \
+    # - H
+    # 'referer: https://www.palloliitto.fi/otteluseuranta/1491645' \
+    # - H
+    # 'accept-language: en-US,en;q=0.9,fi;q=0.8' \
+    # - H
+    # 'cookie: __cfduid=d421efc69e7fcea1324540618536cccb71589645357; has_js=1' \
+    # - -data - raw
+    # 'postdata=1491645' \
+    # - -compressed
+
 
     venue_site = requests.get(url)
     # venue name
@@ -79,7 +110,7 @@ def separate_ottelut(ottelut):
             match.find("div", class_="match-time-wrapper").get_text()
         )
         match_link = base_url + match.select(".live-match-link")[0].a["data-details-href"]
-        # splits the match_link "/otteluseuranta/1491717" -> ["",otteluseuranta,1491717]
+        # splits the match_link "base_url/otteluseuranta/1491717" -> ["",otteluseuranta,1491717]
         match_id = match_link.split("/")[2]
         home_crest_url = (
             match.select(".home-team-wrapper")[0].img["src"]
---
+++ pallo-scraper2.py
@@ -0,0 +1,83 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+
+base_url = "https://www.palloliitto.fi"
+scrape_url = "https://www.palloliitto.fi/seura/3436"
+
+all_ottelut = {}
+
+def scrape_ottelut(url):
+
+    response = requests.get(url)
+
+    if response.status_code != requests.codes.ok:
+        # page gave not 200 response so exit?
+        print("Failed to get match data")
+        return False
+    # else continue!
+
+    page = BS(response.text, "html.parser")
+    # all elements with matching class in a bs4.element.ResultSet
+    ottelut = page.find_all("div", class_="match-row")
+    print(f"found: {len(ottelut)} matches")
+
+    return ottelut
+
+
+def fetch_match_data(match_id):
+
+    # palloliitto api url
+    match_data_url = "https://www.palloliitto.fi/get-match-data"
+    data = {
+        'postdata': match_id,
+    }
+    match_data = requests.post(match_data_url, data=data)
+    match_data_response = match_data.json()["call"]
+    match_data_value = "ERROR"
+    if match_data_response["status"] == "ok":
+        # gets match data values as "type = <class 'dict'>"
+        match_data_value = match_data.json()["match"]
+    else:
+        try:
+            match_data_value = (
+                f"WARNING: status: {match_data_response['status']} - "
+                f"msg: {match_data_response['error_message']}")
+        except KeyError:
+            # this is here in case there are other status messages besided:
+            # "ok" and "error"
+            match_data_value = match_data_response
+
+    return (match_data_value)
+
+
+def fetch_match_details(ottelut):
+
+    for match in ottelut:
+
+        # splits the match_link "`base_url`/otteluseuranta/1491717" -> ["",otteluseuranta,1491717]
+        match_link = base_url + match.select(".live-match-link")[0].a["data-details-href"]
+        # gives the id for the match:
+        match_id = match_link.split("/")[4]
+        print(f"ID: {match_id}, type: {type(match_id)}")
+        # @ToDo [FEAT] Check from database if match details already exist?
+        # @Todo [FEAT] Also need to take in account if any details have changed...?
+        match_details = fetch_match_data(match_id)
+        # error handling if only a string was returned -> no match_details found
+        if not isinstance(match_details, str):
+            match_details["is_kapyla_home"] = True if match_details["club_A_id"] == "3436" else False
+            match_details["match_link"] = match_link
+
+        # @Todo [FEAT] Venue link can be build from data -> https://www.palloliitto.fi/areena/336 -> 'venue_location_id'
+        all_ottelut[match_id] = match_details
+        print("Match details added")
+        break
+
+def scraper():
+
+    fetch_match_details(scrape_ottelut(scrape_url))
+    # print(all_ottelut["1507270"]["match_link"])
+
+# print(fetch_match_data("14916453"))
+
+# scraper()
