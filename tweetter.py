f1b39e1246175b0578b48b205bbe70953b628030
Antti Oksman <antti.oksman101@gmail.com>
2020-08-07 13:24:07 +0300

    basic tweetter

--- tweetter.py
+++ tweetter.py
@@ -1,5 +1,16 @@
+import conf
+import tweepy
 
 
 class Tweetter():
 
-    pass
+    # init twitter client
+    def __init__(self):
+        auth = tweepy.OAuthHandler(conf.twt_api_key, conf.twt_api_secret)
+        auth.set_access_token(conf.twt_access_tkn, conf.twt_access_tkn_secret)
+        self.twt = tweepy.API(auth)
+
+    def tweet_match(self, tweet_text):
+
+        # @ToDo add match url as "attachment_url"
+        self.twt.update_status(tweet_text)
