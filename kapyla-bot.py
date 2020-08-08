b3e97cc9bf7bc125c1bf4820b2721fed9c9fe6cc
Antti Oksman <antti.oksman101@gmail.com>
2020-08-08 18:08:11 +0300

    Testing done

--- kapyla-bot.py
+++ kapyla-bot.py
@@ -13,12 +13,16 @@ def main():
 
     # Database
     # Create database accessor
+    print("Create DB client")
     mongrel = Mongrel(conf.db_user, conf.db_password, conf.db_address)
     # update database
+    print("Update database with match data")
     mongrel.db_updater(conf.match_json)
     # find next match day
+    print("Find match data")
     mongrel.db_find_next_match()
     # generate tweet
+    print("Generating tweets...")
     TweetBot = Tweetter()
     tweet = ""
     if mongrel.days_until_match is 0:
@@ -28,13 +32,13 @@ def main():
             tweet = TB.generate_tweet()
             del TB
             # tweet for every loop!
-            print(tweet)
+            print("Tweet:", tweet)
 
     # else tweet days until next match
     else:
         # @ToDo rng tekstit tälle ja TweetBuilder luokan päivitys
         tweet = f"Enää {mongrel.days_until_match} päivää jäljellä seuraavaan matsiin!"
-        print(tweet)
+        print("Tweet:", tweet)
 
     print("Daily tweets done!")
 
